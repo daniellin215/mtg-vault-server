0863d953ffcae8c936d348fa410b1ad634172936
Daniel <daniellin215@gmail.com>
2024-02-22 01:14:49 -0800

    Add .vscode settings to gitignore

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
-import fastify from 'fastify'
-import { initializeApp, App, applicationDefault } from 'firebase-admin/app';
-  
+import fastify from "fastify";
+import { initializeApp, App, applicationDefault } from "firebase-admin/app";
+
 const server = fastify();
 
-
-const firebaseApp : App = initializeApp({
+const firebaseApp: App = initializeApp({
   credential: applicationDefault(),
 });
 
+server.get("/ping", async (request, reply) => {
+  return "ponged\n";
+});
 
-server.get('/ping', async (request, reply) => {
-  return 'ponged\n';
-})
-
-server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
+server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log(`Server is listening at ${address}, NODE_ENV: ${process.env.NODE_ENV}`)
+  console.log(
+    `Server is listening at ${address}, NODE_ENV: ${process.env.NODE_ENV}`
+  );
 });
