612daa17174ff99fb1d16d19ce5c6abe31b5d023
Daniel <daniellin215@gmail.com>
2024-02-22 00:33:46 -0800

    Add basic signup route

---
+++ routes/v1/auth/signup.ts
@@ -0,0 +1,25 @@
+// create fastify route for signup
+import {FromSchema} from "json-schema-to-ts";
+import { FastifyInstance } from 'fastify';
+import {getAuth} from 'firebase-admin/auth';
+
+export default async function signup(server: FastifyInstance) {
+  server.post<{Body: FromSchema<typeof signupBody>}>('/v1/auth/signup', {
+    schema: {
+      body: signupBody,
+      response: {
+        201: {
+          type: 'string',
+        },
+      },
+    },
+}, async (request, reply) => {
+    const { email, password } = request.body;
+        return await getAuth()
+        .createUser({
+          email,
+          password,
+        });
+   
+  })
+}
---
+++ schemas/signup.ts
@@ -0,0 +1,8 @@
+const signupBody = {
+  type: 'object',
+  properties: {
+    email: { type: 'string' },
+    password: { type: 'string' },
+  },
+  required: ['email', 'password'],
+} as const; // don't forget to use const !
